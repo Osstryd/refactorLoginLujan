37d7878f9eb02e608068754391368281d967ff78
Osstryd <lujanbrandon43@gmail.com>
2024-04-17 22:04:32 -0300

    cambio de ids del cliente por error

--- src/passport/github-strategy.js
+++ src/passport/github-strategy.js
@@ -4,23 +4,24 @@ import UserDao from "../daos/mongodb/user.dao.js";
 const userDao = new UserDao();
 
 const strategyOptions = {
-    clientID: "Iv1.d3dae0d8b8efb40c",
-    clientSecret: "30af5e72ca9c91d9cbdb368ddc5437e1e412e589",
-    callbackURL: "http://localhost:8080/users/profile-github",
+  clientID: "be411904c6c9a8f7257e",
+  clientSecret: "65e969ba22b77bb190028164b76a19f34838d0c8",
+  callbackURL: "http://localhost:8080/users/profile-github",
 };
 
 const registerOrLogin = async (accessToken, refreshToken, profile, done) => {
-    const email = profile._json.email !== null ? profile._json.email : profile_json.blog;
-    const user = await userDao.getByEmail(email);
-    if (user) return done(null, user);
-    const newUser = await userDao.registerUser({
-        first_name: profile._json.name.split(' ')[0],
-        last_name: profile._json.name.split(' ')[1],
-        email,
-        password: '',
-        isGithub: true
-    });
-    return done(null, newUser);
+  const email =
+    profile._json.email !== null ? profile._json.email : profile_json.blog;
+  const user = await userDao.getByEmail(email);
+  if (user) return done(null, user);
+  const newUser = await userDao.registerUser({
+    first_name: profile._json.name.split(" ")[0],
+    last_name: profile._json.name.split(" ")[1],
+    email,
+    password: "",
+    isGithub: true,
+  });
+  return done(null, newUser);
 };
 
 passport.use("github", new GithubStrategy(strategyOptions, registerOrLogin));
